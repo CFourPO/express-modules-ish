aa76f8386d64c8f945b45bc0e059fcefd4bcf14b
Chris Pomerville <cpomerville@gmail.com>
2018-01-24 17:56:22 -0500

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,20 +7,29 @@ import listController from './lists/list.controller';
 
 const router = express.Router();
 
+/** TODO: replace each sub-route with nested router: 
+ *  
+ * let subRoute = './<sub-route>/index';
+ * router.use('/<sub-route>', subRoute);
+ */
+
+// Bookmark
 router.get('/bookmark', bookmarkController.selectAll);
 router.get('/bookmark/:id', bookmarkController.selectById);
 router.post('/bookmark', bookmarkController.insert);
 
+// Category
 router.get('/category', categoryController.selectAll);
 router.get('/category/:id', categoryController.selectById);
 router.post('/category', categoryController.insert);
 router.put('/category', categoryController.addChild);
-// router.use('/bookmark', bookmarkController);
 
+// User
 router.get('/user', userController.selectAll);
 router.get('/user/:id', userController.selectById);
 router.post('/user', userController.insert);
 
+// List
 router.get('/list', listController.selectAll);
 router.get('/list/:id', listController.selectById);
 router.post('/list', listController.insert);
